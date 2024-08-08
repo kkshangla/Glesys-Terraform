560bf76fc9cac83e0cfe2b30cb9f626abffeec98
kkshangla <44677746+kkshangla@users.noreply.github.com>
2024-08-08 16:51:19 +0200

    added storage

--- main.tf
+++ main.tf
@@ -5,5 +5,35 @@ resource "glesys_server" "server" {
   cpu        = var.cpu
   storage    = var.storage
   bandwidth  = var.bandwidth
+
+  cloudconfig = <<EOF
+#cloud-config
+users:
+  - name: ${var.glesys_user}
+    ssh-authorized-keys:
+      - ${join("\n      - ", var.glesys_ssh_keys)}
+    sudo: ['ALL=(ALL) NOPASSWD:ALL']
+    shell: /bin/bash
+EOF
 }
 
+resource "glesys_objectstorage_instance" "my_object_storage" {
+  datacenter  = var.storage_location
+  description = var.storage_description
+}
+
+output "object_storage_accesskey" {
+  value = glesys_objectstorage_instance.my_object_storage.accesskey
+}
+
+output "object_storage_secretkey" {
+  value = glesys_objectstorage_instance.my_object_storage.secretkey
+}
+
+output "object_storage_id" {
+  value = glesys_objectstorage_instance.my_object_storage.id
+}
+
+output "object_storage_created" {
+  value = glesys_objectstorage_instance.my_object_storage.created
+}
--- variables.tf
+++ variables.tf
@@ -1,45 +1,63 @@
 variable "glesys_token" {
   description = "User token for the GleSYS API"
   type        = string
+  default = "Enter here Glesys api"
 }
 
 variable "glesys_userid" {
   description = "User ID for the GleSYS API"
   type        = string
+  default = "Enter here User ID"
 }
+
+variable "glesys_ssh_keys" {
+  description = "SSH keys for the GleSYS server"
+  type        = list(string)
+  default = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDGZvKd1do/82lHLIn57GgUX8Jl1RVix5d9XQ2V7iFjlASuqFCnqHRWvNflMLP4znVnOYmGeR9CANwNQY/Xs5q8KgzJhPmpdZR/sYLyOTkiuH/2EVUaXhISZS6IgqFesSyZxZTp/duA4SHluLVOXgh+vDnG7FCT5FUdA6dgaouqlV+vuP/HB0+SRPGKA7H4c+Bi4fSYaoJb7nGtPZVF2/Hk7a067OjgXtZN3zkSmV3fO6WftE4K9Zlg+UbOtdXznRZHKTSmDCQtCEf5uNrm/H2d0gkxpJDib4VHsiEnPvf7dzTR5mh2ZP8SPiCcGExijd29R3oQqynI2uwvk7mjQr1Njpjm6NB/zsYGtlDv03UN3F368H2qeUvviLDmQWqwq4vqsf8yqWfZib4EjX+JpeoIp6bF5qElzK1qOJGbgUbeuFyrx+VCW5NcAGg2asKPbovwoy7AYKK+od2CRg/uY1+zXiWXb9x30LONXt2wf0GyTlSp/EREtarhgFlClMTPnZc= khalidkhan@kk" ]
+}
+
 
 variable "hostname" {
   description = "Hostname for the server"
   type        = string
-  default     = "Hantverkshjalpen-production"
+}
+
+variable "glesys_user" {
+  description = "Username for the GleSYS server"
+  type        = string
 }
 
 variable "datacenter" {
   description = "Datacenter to host the server in"
   type        = string
-  default     = "Stockholm"
 }
 
 variable "memory" {
   description = "Amount of memory for the server (MB)"
   type        = number
-  default     = 2048
 }
 
 variable "cpu" {
   description = "Number of CPUs for the server"
   type        = number
-  default     = 2
 }
 
 variable "storage" {
   description = "Amount of storage for the server (GB)"
   type        = number
-  default     = 50
 }
 
 variable "bandwidth" {
   description = "Amount of bandwidth for the server (GB)"
   type        = number
-  default     = 100
+}
+
+variable "storage_location" {
+  description = "The location of the object storage"
+  type        = string
+}
+
+variable "storage_description" {
+  description = "Description of the object storage"
+  type        = string
 }
